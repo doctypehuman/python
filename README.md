59ca142388badd0a98f25273182cdfe997768889
doctypehuman <doctypehuman@gmail.com>
2021-06-11 07:00:21 +0200

    first few lines

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python
 My journey in python
+has begun on Fri Jun 11 06:40:23 CEST 2021
---
+++ f1.py
@@ -0,0 +1,16 @@
+# My first program in python
+print('Please enter your password ')
+
+myName = input()
+
+print('Checking your input now')
+
+print('The length of your password is :')
+
+print(len(myName))
+
+print('Any random number')
+
+myAge = input()
+
+print('Your number plus one is : ' + str(int(myAge) + 99) )
